424fe6815effa67c4e15febd81b554cbf80c42dc
yangyangma <yangyangma2016@gmail.com>
2019-06-14 11:10:27 -0400

    sum

---
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText1,editText2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+
+        initialize();
+    }
+
+    private void initialize() {
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+    }
+
+    public void doSum(View view) {
+        String editText1_str = editText1.getText().toString();
+        String editText2_str = editText2.getText().toString();
+
+        int editText1_int = Integer.valueOf(editText1_str);
+        int editText2_int = Integer.valueOf(editText2_str);
+
+        int sum = editText1_int + editText2_int;
+
+        Toast.makeText(this, String.valueOf(sum), Toast.LENGTH_LONG).show();
+    }
+}
