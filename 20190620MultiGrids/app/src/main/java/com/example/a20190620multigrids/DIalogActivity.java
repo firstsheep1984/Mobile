8a219332c9df44f25901f8b134a4118ee14afd1d
yangyangma <yangyangma2016@gmail.com>
2019-06-20 10:51:55 -0400

    alert dialog

--- 20190620MultiGrids/app/src/main/java/com/example/a20190620multigrids/DIalogActivity.java
+++ 20190620MultiGrids/app/src/main/java/com/example/a20190620multigrids/DIalogActivity.java
@@ -1,14 +1,153 @@
 package com.example.a20190620multigrids;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class DIalogActivity extends AppCompatActivity {
+public class DIalogActivity extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnClickListener {
+    Button button_builder1, button_builder2, button_builder3, button_builder4, button_progress, button_custom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+        initialize();
     }
+
+    private void initialize() {
+        button_builder1 = findViewById(R.id.button_builder1);
+        button_builder1.setOnClickListener(this);
+        button_builder2 = findViewById(R.id.button_builder2);
+        button_builder2.setOnClickListener(this);
+        button_builder3 = findViewById(R.id.button_builder3);
+        button_builder3.setOnClickListener(this);
+
+        button_builder4 = findViewById(R.id.button_builder4);
+        button_builder4.setOnClickListener(this);
+
+        button_progress = findViewById(R.id.button_progress);
+        button_progress.setOnClickListener(this);
+
+        button_custom = findViewById(R.id.button_custom);
+        button_custom.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        switch (i) {
+            case DialogInterface.BUTTON_POSITIVE:
+                Toast.makeText(DIalogActivity.this, "id = " + i, Toast.LENGTH_SHORT).show();
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                Toast.makeText(DIalogActivity.this, "id = " + i, Toast.LENGTH_SHORT).show();
+                break;
+            case DialogInterface.BUTTON_NEUTRAL:
+                Toast.makeText(DIalogActivity.this, "id = " + i, Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int btnId = view.getId();
+        switch (btnId) {
+            case R.id.button_builder1:
+                builder1();
+                break;
+            case R.id.button_builder2:
+                builder2();
+                break;
+            case R.id.button_builder3:
+                builder3();
+                break;
+            case R.id.button_builder4:
+                builder4();
+                break;
+            case R.id.button_progress:
+                // progress();
+                break;
+            case R.id.button_custom:
+                //custom();
+                break;
+
+        }
+    }
+
+
+
+    private void builder1() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("This is my title");
+
+        // second argument in below methods is a reference to event listener
+        alertDialogBuilder.setPositiveButton("yes", this);
+        alertDialogBuilder.setNegativeButton("No", this);
+        alertDialogBuilder.setNeutralButton("Neutral", this);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void builder2() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // type 2
+        builder.setTitle("AlertDialog\njust a question for demo")
+                .setMessage("Do you want to delete this file?")
+                .setCancelable(false)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                // we just define event listener for yes button
+                // but it can be defined for 'no' and 'cancel'
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(DIalogActivity.this, "id = " + i, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .setNeutralButton("Cancel", null);
+        builder.show();
+    }
+
+    private void builder3() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // type 3
+        builder.setTitle("Question ?")
+                .setCancelable(false)
+                .setSingleChoiceItems(new String[]{"A", "B", "C", "D"}, //single choice
+                        0, //-1 for no default selection
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(DIalogActivity.this, "id = " + i, Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setPositiveButton("OK", null);
+        builder.show();
+    }
+    private void builder4() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // type 4
+        builder.setTitle("Question ?")
+                .setCancelable(true)
+                .setMultiChoiceItems(new String[]{"item0", "item1", "item2", "item3", "item4", "item5"},
+                        new boolean[]{false, true, false, false, true, false},
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                                Toast.makeText(DIalogActivity.this, "id = " + i, Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setPositiveButton("OK", null);
+        builder.show();
+    }
+
 }
--- 20190620MultiGrids/app/src/main/java/com/example/a20190620multigrids/MainActivity.java
+++ 20190620MultiGrids/app/src/main/java/com/example/a20190620multigrids/MainActivity.java
@@ -2,13 +2,98 @@ package com.example.a20190620multigrids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button_dialog, button_toasts, button_internet, button_async, button_spinner, button_listview, button_file, buttondb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+        button_dialog = findViewById(R.id.button_dialog);
+        button_dialog.setOnClickListener(this);
+        button_async = findViewById(R.id.button_async);
+        button_async.setOnClickListener(this);
+        button_internet = findViewById(R.id.button_internet);
+        button_internet.setOnClickListener(this);
+        button_spinner = findViewById(R.id.button_spinner);
+        button_spinner.setOnClickListener(this);
+        button_listview = findViewById(R.id.button_listview);
+        button_listview.setOnClickListener(this);
+        button_toasts = findViewById(R.id.button_toast);
+        button_toasts.setOnClickListener(this);
+        button_file = findViewById(R.id.button_file);
+        button_file.setOnClickListener(this);
+        buttondb = findViewById(R.id.button_DB);
+        buttondb.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int btnId = view.getId();
+        switch (btnId) {
+            case R.id.button_dialog:
+                dialog();
+                break;
+            case R.id.button_async:
+                async();
+                break;
+            case R.id.button_internet:
+                internet();
+                break;
+            case R.id.button_spinner:
+                spinner();
+                break;
+            case R.id.button_listview:
+                listview();
+                break;
+            case R.id.button_toast:
+                toast();
+                break;
+            case R.id.button_file:
+                file();
+                break;
+            case R.id.button_DB:
+                db();
+                break;
+        }
+    }
+
+
+    private void dialog() {
+        //  String lastName = editText_LastName.getText().toString();
+        Intent myIntent = new Intent(this, DIalogActivity.class);
+        // myIntent.putExtra("lastName", lastName);
+        startActivity(myIntent);
+    }
+
+    private void async() {
+    }
+
+    private void internet() {
+    }
+
+    private void spinner() {
+    }
+
+    private void toast() {
+    }
+
+    private void listview() {
+    }
+
+    private void file() {
+    }
+
+    private void db() {
     }
 }
